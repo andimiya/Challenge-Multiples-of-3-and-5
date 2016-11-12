5fd7843886103479060d13034cd731ebd003b2b1
Andrea <takamiya.andrea@gmail.com>
2016-11-12 09:54:23 -1000

    For loops are pushing multiples of 3 or 5 to an array, not multiples of both

--- multiples.js
+++ multiples.js
@@ -6,7 +6,47 @@
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
+  var sumArray = [];
   // do your work here
+
+  //3 * x (1) = sum
+  //3 * x (2)  = sum
+  //up until sum < n
+  //add sum to array
+
+  //var x = x-3 Then add the result to the array
+  //n-5 add to array, until 0
+
+
+
+    for (var x = 0; x <= 4; x++) {
+      if (sum < n){
+        var sum = 3 * x;
+        sumArray.push(sum);
+
+      }
+
+    }
+
+    for (var x = 0; x <=4; x++) {
+      if (sum < n) {
+        var sum = 5 * x;
+        sumArray.push(sum2);
+      }
+
+    }
+    console.log(sumArray);
+
+
+
+
+
+  // var x = x-3;
+  // sumArray.push(x);
+  // var x = x-3;
+  // sumArray.push(x);
+  // console.log(sumArray);
+
 
   return sum;
 };
